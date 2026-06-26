0304eb66371dbaaefd0a90fd5b24638f2f3b1a9b
Divya Koshti <koshtidivya2004@gmail.com>
2026-06-26 18:39:00 +0530

    Update cors

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://sweettalk-chatapp.netlify.app/",
+      "https://sweettalk-chatapp.netlify.app",
     ],
     credentials: true,
   })
